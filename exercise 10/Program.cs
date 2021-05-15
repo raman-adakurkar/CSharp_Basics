8781465fce8b24695f95c04ed26b32efec354630
ramanLadakurkar <ramanadakurkar@gmail.com>
2021-05-15 11:42:55 +0530

    exercise 10 solved

---
+++ exercise 10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and ask the user to enter their name.
+            //Use an array to reverse the name and then store the result in a new string.
+            //Display the reversed name on the console.
+
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+
+            char[] arrName = name.ToCharArray();
+
+            Array.Reverse(arrName);
+            foreach (var names in arrName)
+            {
+                Console.Write(names);
+            }
+            Console.WriteLine();
+
+            
+        }
+    }
+}
