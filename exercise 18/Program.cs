25544f373ac45e688a1eb6f722cb425ffaf5e145
ramanLadakurkar <ramanadakurkar@gmail.com>
2021-05-20 11:21:04 +0530

    Exercise 18 solved

---
+++ exercise 18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and ask the user to enter an English word.
+            //Count the number of vowels (a, e, o, u, i) in the word.
+            //So, if the user enters "inadequate", the program should display 6 on the console.
+
+            Console.Write("Enter an English Word : ");
+            var word = Console.ReadLine().ToLower();
+
+            var vowels = new List<char>(new char[] { 'a', 'e', 'i', 'o', 'u' });
+            var vowelsCount = 0;
+
+            foreach (var character in word)
+            {
+                if (vowels.Contains(character))
+                {
+                    vowelsCount++;
+                }
+            }
+
+            Console.WriteLine(vowelsCount);
+
+
+
+        }
+    }
+}
