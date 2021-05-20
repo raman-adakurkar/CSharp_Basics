db89d40c83cf6d692c20ea04bed45e10915cb56d
ramanLadakurkar <ramanadakurkar@gmail.com>
2021-05-20 12:17:41 +0530

    exercise 19 solved

---
+++ exercise 19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace exercise_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads a text file and displays the number of words.
+
+            var path = @"C:\Users\raman\Desktop\raman.txt";
+            var wordNumber = new List<string>(File.ReadAllText(path).Split(' '));
+            Console.WriteLine(wordNumber.Count);
+           
+        }
+    }
+}
